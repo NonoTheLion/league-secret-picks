9ffaffba1ee1b9b241304e8af036f55648076285
Nono <noe.d-aboville@epita.fr>
2022-09-14 22:50:38 +0200

    base set

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ChampionsComponent } from './champions/champions.component';
+import { BottomPageComponent } from './bottom-page/bottom-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChampionsComponent,
+    BottomPageComponent,
   ],
   imports: [
     BrowserModule
