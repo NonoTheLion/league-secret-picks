61e5d2ce3d1ec264171f70bddee064b9b74b214a
Nono <noe.d-aboville@epita.fr>
2022-09-15 12:17:04 +0200

    ajout couleurs selon le mode de jeu + petits changement css

--- src/app/champion-sheet.ts
+++ src/app/champion-sheet.ts
@@ -3,6 +3,7 @@ export interface ChampionSheet {
     url: string;
     difficulty: number; 
     viability: number; 
+    gamemode: string; // aram, invoc, special, all
 }
 
 export const champions = [
@@ -10,18 +11,21 @@ export const champions = [
         name: 'Maitre Yi Quantique',
         url: 'https://raw.githubusercontent.com/NonoTheLion/league-secret-picks/ressources/pdf/QuantiqueYi.pdf',
         difficulty: 1,
-        viability: 4
+        viability: 4,
+        gamemode: 'aram'
     },
     {
         name: 'Qiyanus Maximus',
         url: '',
         difficulty: 5,
-        viability: 5
+        viability: 5,
+        gamemode: 'all'
     },
     {
         name: 'Khartus Capitaliste',
         url: '',
         difficulty: 4,
-        viability: 4
+        viability: 4,
+        gamemode: 'all'
     }
 ]
